8ca6c2cdb42295c10efb4aacbb784969698e3bb7
wwwht <wht_no1@outlook.com>
2023-06-29 16:04:52 +0800

    update resolve

---
+++ config/total_config.py
@@ -0,0 +1,20 @@
+'''
+Description: 12 project total configure file
+Version: 2.0
+Autor: wht
+Date: 2023-06-29 11:20:33
+LastEditors: wht
+LastEditTime: 2023-06-29 14:06:27
+'''
+
+def get_my_sql_config():
+    return {
+        "host": "173.7.24.182",
+        "database": "siwei",
+        "port": 3306,
+        "username": "root",
+        "password": "MySQL_2023_sh12j"
+    }
+    
+    
+    
